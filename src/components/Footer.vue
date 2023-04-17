1d00a7793e444b7c950bdfec7c6315c310b1cef2
RuggeroCoco <ruggerococo94@gmail.com>
2023-04-17 20:57:35 +0200

    First commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,76 @@
+<script>
+import img1 from "../assets/buy-comics-digital-comics.png";
+import img2 from "../assets/buy-comics-merchandise.png";
+import img3 from "../assets/buy-comics-subscriptions.png";
+import img4 from "../assets/buy-comics-shop-locator.png";
+import img5 from "../assets/buy-dc-power-visa.svg";
+
+export default {
+  data() {
+    return {
+      links: [
+        {
+          title: "Digital Comics",
+          icon: img1,
+          url: "/",
+        },
+        {
+          title: "Digital Comics",
+          icon: img2,
+          url: "/",
+        },
+        {
+          title: "Digital Comics",
+          icon: img3,
+          url: "/",
+        },
+        {
+          title: "Digital Comics",
+          icon: img4,
+          url: "/",
+        },
+        {
+          title: "Digital Comics",
+          icon: img5,
+          url: "/",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="buy-comics">
+    <div v-for="(link, index) in links" :key="index.toString()">
+      <a class="buy-comics_link" :href="link.url">
+        <img :src="link.icon" alt="" />
+        <p>{{ link.title }}</p>
+      </a>
+    </div>
+  </div>
+  <div></div>
+  <div></div>
+</template>
+
+<style scoped lang="scss">
+.buy-comics {
+  background-color: blue;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 40px 10vw;
+
+  &_link {
+    display: flex;
+    align-items: center;
+    color: white;
+    img {
+      max-height: 80px;
+      max-width: 60px;
+      margin-right: 20px;
+      object-fit: contain;
+    }
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+  data() {
+    return {
+      buttons: [
+        {
+          title: "Characters",
+          url: "/",
+          current: false,
+        },
+        {
+          title: "Characters",
+          url: "/",
+          current: false,
+        },
+        {
+          title: "Characters",
+          url: "/",
+          current: false,
+        },
+        {
+          title: "Characters",
+          url: "/",
+          current: false,
+        },
+        {
+          title: "Characters",
+          url: "/",
+          current: false,
+        },
+        {
+          title: "Characters",
+          url: "/",
+          current: false,
+        },
+        {
+          title: "Characters",
+          url: "/",
+          current: false,
+        },
+        {
+          title: "Characters",
+          url: "/",
+          current: false,
+        },
+        {
+          title: "Characters",
+          url: "/",
+          current: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="header">
+    <img src="../assets/dc-logo.png" alt="Logo" />
+    <div v-for="(button, index) in buttons" :key="index.toString()">
+      <input type="button" :value="button.title" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
